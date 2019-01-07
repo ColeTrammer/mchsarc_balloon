671b6afc0300e0bc2fa735c0c8e175ca5d95c090
Cole <cole.trammer@gmail.com>
2019-01-07 14:01:01 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 from sense_hat import SenseHat
+import time
 
 sense = SenseHat()
+f = open("data.csv", "w")
+f.write("Temperature,Humidity,Pressure,Pitch,Roll,Yaw,Acceleration X,Acceleration Y,Acceleration X")
+
+def write_data():
