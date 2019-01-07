a7697dee18378ebad2b519b501daf325a99fa73f
Cole <cole.trammer@gmail.com>
2019-01-07 14:11:25 -0800

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ from sense_hat import SenseHat
 import time
 
 sense = SenseHat()
+sense.set_imu_config(False, True, True)
 f = open("data.csv", "w")
-f.write("Temperature,Humidity,Pressure,Pitch,Roll,Yaw,Acceleration X,Acceleration Y,Acceleration X")
+f.write("Temperature,Humidity,Pressure,Pitch,Roll,Yaw,Acceleration X,Acceleration Y,Acceleration X\n")
 
 def write_data():
-  f.write(f"{sense.get_temperature()},{sense.get_humidity},{sense.get_pressure()},{sense.get_orientation()["pitch"]},{sense.get_orientation()["roll"]},{sense.get_orientation()["yaw"]},{sense.get_acceleromter_raw()["x"]},{sense.get_acceleromter_raw()["y"]},{sense.get_acceleromter_raw()["z"]}")
+  f.write(f"{sense.get_temperature()},{sense.get_humidity},{sense.get_pressure()},{sense.get_orientation()["pitch"]},{sense.get_orientation()["roll"]},{sense.get_orientation()["yaw"]},{sense.get_acceleromter_raw()["x"]},{sense.get_acceleromter_raw()["y"]},{sense.get_acceleromter_raw()["z"]}\n")
 
 start_time = time.time()
 interval = 3.0 # seconds
