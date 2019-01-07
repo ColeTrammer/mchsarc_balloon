2d142502d2aa704b4337745e5f7d2bad8e887c33
Cole <cole.trammer@gmail.com>
2019-01-07 14:06:39 -0800

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,4 @@ f = open("data.csv", "w")
 f.write("Temperature,Humidity,Pressure,Pitch,Roll,Yaw,Acceleration X,Acceleration Y,Acceleration X")
 
 def write_data():
+  f.write(f"{sense.get_temperature()},{sense.get_humidity},{sense.get_pressure()},{sense.get_orientation()["pitch"]},{sense.get_orientation()["roll"]},{sense.get_orientation()["yaw"]},{sense.get_acceleromter_raw()["x"]},{sense.get_acceleromter_raw()["y"]},{sense.get_acceleromter_raw()["z"]}")
